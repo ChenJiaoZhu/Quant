7fc12335546c10bdc85122fd05d5a10be6c6a9b4
ChenJiaoZhu <30440891+ChenJiaoZhu@users.noreply.github.com>
2017-08-27 16:08:27 +0800

    record the time

--- ml_q/regression/model_selection.py
+++ ml_q/regression/model_selection.py
@@ -2,6 +2,7 @@
 # !/usr/bin/python
 # model_selection.py
 
+import time
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVR, LinearSVR
@@ -22,6 +23,7 @@ def lasso_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 10
+    start = time.time()
     for a in [0.0004, 0.0003, 0.0002, 0.003, 0.05]:
         # Set the parameters by cross-validation
         tuned_parameters = [{'alpha': [a]}]
@@ -29,17 +31,19 @@ def lasso_grid_search(X, y):
         # Perform the grid search on the tuned parameters
         model = GridSearchCV(Lasso(tol=1e-5), tuned_parameters, cv=cv, n_jobs=2)
         model.fit(X, y)
+        end = time.time()
 
         for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                               model.cv_results_['params']):
-            print "%0.8f for %r    [X.shape=%s, cv=%s]" % \
-                  (mean_test_score, params, str(X.shape), cv)
+            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
+                  (mean_test_score, params, str(X.shape), cv, (end-start)/60)
 
 
 def ridge_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 10
+    start = time.time()
     for a in [0.11, 0.12, 0.13, 0.14, 0.15, 0.16]:
         # Set the parameters by cross-validation
         tuned_parameters = [{'alpha': [a]}]
@@ -47,17 +51,19 @@ def ridge_grid_search(X, y):
         # Perform the grid search on the tuned parameters
         model = GridSearchCV(Ridge(), tuned_parameters, cv=cv, n_jobs=1)
         model.fit(X, y)
+        end = time.time()
 
         for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                               model.cv_results_['params']):
-            print "%0.8f for %r    [X.shape=%s, cv=%s]" % \
-                  (mean_test_score, params, str(X.shape), cv)
+            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
+                  (mean_test_score, params, str(X.shape), cv, (end-start)/60)
 
 
 def lsvr_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 10
+    start = time.time()
     for c in [9, 11, 12, 17, 25]:
         # Set the parameters by cross-validation
         tuned_parameters = [{'C': [c]}]
@@ -65,17 +71,19 @@ def lsvr_grid_search(X, y):
         # Perform the grid search on the tuned parameters
         model = GridSearchCV(LinearSVR(), tuned_parameters, cv=cv, n_jobs=2)
         model.fit(X, y)
+        end = time.time()
 
         for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                               model.cv_results_['params']):
-            print "%0.8f for %r    [X.shape=%s, cv=%s]" % \
-                  (mean_test_score, params, str(X.shape), cv)
+            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
+                  (mean_test_score, params, str(X.shape), cv, (end-start)/60)
 
 
 def svr_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 3
+    start = time.time()
     for g in [1e-2, 1e-5]:
         for c in [1, 10, 100, 1000, 10000]:
             # Set the parameters by cross-validation
@@ -84,17 +92,19 @@ def svr_grid_search(X, y):
             # Perform the grid search on the tuned parameters
             model = GridSearchCV(SVR(), tuned_parameters, cv=cv, n_jobs=2)
             model.fit(X, y)
+            end = time.time()
 
             for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                                   model.cv_results_['params']):
-                print "%0.8f for %r    [X.shape=%s, cv=%s]" % \
-                      (mean_test_score, params, str(X.shape), cv)
+                print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
+                      (mean_test_score, params, str(X.shape), cv, (end-start)/60)
 
 
 def etr_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 3
+    start = time.time()
     for n in [200, 500, 1000, 2000]:
         # Set the parameters by cross-validation
         tuned_parameters = [{'n_estimators': [n]}]
@@ -105,16 +115,19 @@ def etr_grid_search(X, y):
         # Perform the grid search on the tuned parameters
         model = GridSearchCV(ExtraTreesRegressor(n_jobs=2), tuned_parameters, cv=cv)
         model.fit(X, y)
+        end = time.time()
 
         for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                               model.cv_results_['params']):
-            print "%0.8f for %r    [X.shape=%s, cv=%s]" % \
-                  (mean_test_score, params, str(X.shape), cv)
+            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
+                  (mean_test_score, params, str(X.shape), cv, (end-start)/60)
+
 
 def rfr_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 3
+    start = time.time()
     for n in [200, 500, 1000, 2000]:
         # Set the parameters by cross-validation
         tuned_parameters = [{'n_estimators': [n]}]
@@ -125,9 +138,10 @@ def rfr_grid_search(X, y):
         # Perform the grid search on the tuned parameters
         model = GridSearchCV(RandomForestRegressor(n_jobs=2), tuned_parameters, cv=cv)
         model.fit(X, y)
+        end = time.time()
 
         for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                               model.cv_results_['params']):
-            print "%0.8f for %r    [X.shape=%s, cv=%s]" % \
-                  (mean_test_score, params, str(X.shape), cv)
+            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
+                  (mean_test_score, params, str(X.shape), cv, (end-start)/60)
 
