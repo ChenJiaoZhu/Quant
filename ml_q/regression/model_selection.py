5680822a4a5b4f7b8b46b3ba3afeac5961eb3f47
ChenJiaoZhu <30440891+ChenJiaoZhu@users.noreply.github.com>
2017-08-28 15:37:17 +0800

    fix time calculation

--- ml_q/regression/model_selection.py
+++ ml_q/regression/model_selection.py
@@ -23,8 +23,8 @@ def lasso_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 10
-    start = time.time()
     for a in [0.0004, 0.0003, 0.0002, 0.003, 0.05]:
+        start = time.time()
         # Set the parameters by cross-validation
         tuned_parameters = [{'alpha': [a]}]
 
@@ -35,16 +35,16 @@ def lasso_grid_search(X, y):
 
         for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                               model.cv_results_['params']):
-            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
-                  (mean_test_score, params, str(X.shape), cv, (end-start)/60)
+            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f s" % \
+                  (mean_test_score, params, str(X.shape), cv, (end-start))
 
 
 def ridge_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 10
-    start = time.time()
     for a in [0.11, 0.12, 0.13, 0.14, 0.15, 0.16]:
+        start = time.time()
         # Set the parameters by cross-validation
         tuned_parameters = [{'alpha': [a]}]
 
@@ -55,16 +55,16 @@ def ridge_grid_search(X, y):
 
         for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                               model.cv_results_['params']):
-            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
-                  (mean_test_score, params, str(X.shape), cv, (end-start)/60)
+            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f s" % \
+                  (mean_test_score, params, str(X.shape), cv, (end-start))
 
 
 def lsvr_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 10
-    start = time.time()
     for c in [9, 11, 12, 17, 25]:
+        start = time.time()
         # Set the parameters by cross-validation
         tuned_parameters = [{'C': [c]}]
 
@@ -75,17 +75,17 @@ def lsvr_grid_search(X, y):
 
         for mean_test_score, params in zip(model.cv_results_['mean_test_score'],
                                               model.cv_results_['params']):
-            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f min" % \
-                  (mean_test_score, params, str(X.shape), cv, (end-start)/60)
+            print "%0.8f for %r    [X.shape=%s, cv=%s]  %0.2f s" % \
+                  (mean_test_score, params, str(X.shape), cv, (end-start))
 
 
 def svr_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 3
-    start = time.time()
     for g in [1e-2, 1e-5]:
         for c in [1, 10, 100, 1000, 10000]:
+            start = time.time()
             # Set the parameters by cross-validation
             tuned_parameters = [{'kernel': ['rbf'], 'gamma': [g], 'C': [c]}]
 
@@ -104,8 +104,8 @@ def etr_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 3
-    start = time.time()
     for n in [200, 500, 1000, 2000]:
+        start = time.time()
         # Set the parameters by cross-validation
         tuned_parameters = [{'n_estimators': [n], 'max_features': ['sqrt']}]
         # tuned_parameters = [{'n_estimators': [200, 500, 1000],
@@ -127,8 +127,8 @@ def rfr_grid_search(X, y):
 
     print "R^2 scores calculated on training set:"
     cv = 3
-    start = time.time()
     for n in [200, 500, 1000, 2000]:
+        start = time.time()
         # Set the parameters by cross-validation
         tuned_parameters = [{'n_estimators': [n], 'max_features': ['sqrt']}]
         # tuned_parameters = [{'n_estimators': [200, 500, 1000],
@@ -149,10 +149,10 @@ def rfr_grid_search(X, y):
 def etr_search(X_train, X_test, y_train, y_test):
 
     print "R^2 scores calculated on test set:"
-    start = time.time()
     n_jobs = 2
     cv = 0
     for n in [20, 100, 200, 500, 1000, 1500, 2000]:
+        start = time.time()
         # tuned_parameters = [{'n_estimators': [200, 500, 1000],
         #                      'max_features': ['auto', 'log2'],
         #                      'min_samples_leaf': [1, 10, 50]}]
@@ -168,10 +168,10 @@ def etr_search(X_train, X_test, y_train, y_test):
 def rfr_search(X_train, X_test, y_train, y_test):
 
     print "R^2 scores calculated on test set:"
-    start = time.time()
     n_jobs = 2
     cv = 0
     for n in [20, 100, 200, 500, 1000, 1500, 2000]:
+        start = time.time()
         # tuned_parameters = [{'n_estimators': [200, 500, 1000],
         #                      'max_features': ['auto', 'log2'],
         #                      'min_samples_leaf': [1, 10, 50]}]
