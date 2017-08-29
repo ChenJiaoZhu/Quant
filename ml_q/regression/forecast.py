606d4564923a40cbb2f31a98f475c69666fdd7ac
ChenJiaoZhu <30440891+ChenJiaoZhu@users.noreply.github.com>
2017-08-29 12:14:23 +0800

    rfr param space is finished

--- ml_q/regression/forecast.py
+++ ml_q/regression/forecast.py
@@ -10,19 +10,16 @@ def forecast(X_train, X_test, y_train, y_test):
 
     # Create the (parametrised) models
     models = [("LR", LinearRegression()),
-              ("Ridge", Ridge(alpha=34)),
-              ("Lasso", Lasso(alpha=0.012)),
-              ("LSVR", LinearSVR(C=1.0)),
-              ("SVR", SVR(C=10000.0, cache_size=200, max_iter=-1,
-                          coef0=0.0, degree=3, gamma=0.00001, kernel='rbf',
-                          shrinking=True, tol=0.001, verbose=False)),
+              ("Ridge", Ridge(alpha=0.09)),
+              ("Lasso", Lasso(alpha=0.0002)),
+              ("LSVR", LinearSVR(C=17.0)),
+              ("SVR", SVR(C=10000.0, gamma=0.00001, kernel='rbf')),
               ("ETR", ExtraTreesRegressor(n_estimators=1000, n_jobs=6,
                                           max_features=0.5, max_depth=15,
                                           min_samples_split=5, min_samples_leaf=3)),
-              ("RFR", RandomForestRegressor(n_estimators=200,
-                  criterion='mse',max_depth=None, min_samples_split=2,
-                  min_samples_leaf=1, max_features='auto',
-                  oob_score=False, n_jobs=2, random_state=None))]
+              ("RFR", RandomForestRegressor(n_estimators=1000, n_jobs=6,
+                                            max_features=0.5, max_depth=20,
+                                            min_samples_split=5, min_samples_leaf=3))]
 
     # Iterate through the models
     model = {}
