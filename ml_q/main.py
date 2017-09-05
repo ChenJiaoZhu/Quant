036b965e9df59a70affaaf99f18afc827270c540
ChenJiaoZhu <30440891+ChenJiaoZhu@users.noreply.github.com>
2017-09-05 12:56:46 +0800

    training model: first part of main function

--- ml_q/main.py
+++ ml_q/main.py
@@ -1,62 +1,43 @@
 # coding: UTF-8
 
 import pandas as pd
-import numpy as np
 from Quant.ml_q import get_data
 from Quant.ml_q.regression.feature_selection import get_best_subset
-from Quant.ml_q.regression.forecast import training_model, predict_by_cv, model_test
-from Quant.ml_q.regression.param_search import easy_search, svr_search, etr_search, rfr_search
-from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.svm import LinearSVR, SVR
+from Quant.ml_q.regression.ensemble_model import get_model, predict_by_cv
+from Quant.ml_q.regression.ensemble_model import training_model, model_predict
+
 
 if __name__ == "__main__":
 
+    # create the training data and test data
     X, y, backtest_X, backtest_y_info = get_data.Get_Data(type_y='reg')
-    X_train, X_test, y_train, y_test = get_data.split_by_weigh(X, y)
-    X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR48 = get_best_subset(X_train)
-    X_test_Ridge78, X_test_Lasso65, X_test_RFR78, X_test_RFR48 = get_best_subset(X_test)
-    models, pred_list = training_model(X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR48,
-                                       X_test_Ridge78, X_test_Lasso65, X_test_RFR78, X_test_RFR48,
-                                       X_train, X_test, y_train, y_test)
-    X_pred = pd.DataFrame(pred_list).T
+    X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR48 = get_best_subset(X)
+    X_test_Ridge78, X_test_Lasso65, X_test_RFR78, X_test_RFR48 = get_best_subset(backtest_X)
 
-    models = [("LR", LinearRegression()),
-              ("Ridge", Ridge(alpha=0.09)),
-              ("Lasso", Lasso(alpha=0.0002)),
-              ("LSVR", LinearSVR(C=17.0)),
-              ("SVR", SVR(C=100.0, gamma=0.01, epsilon=0.1, kernel='rbf')),
-              ("ETR", ExtraTreesRegressor(n_estimators=1000, n_jobs=2,
-                                          max_features=0.5, max_depth=18,
-                                          min_samples_split=6, min_samples_leaf=4)),
-              ("RFR", RandomForestRegressor(n_estimators=1000, n_jobs=2,
-                                            max_features=0.5, max_depth=16,
-                                            min_samples_split=5, min_samples_leaf=2))]
+    # create the parametrised models of each single model and ensemble model
+    single, ensemble = get_model()
 
+    # train each single model
+    models = training_model(X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR48,
+                            X, backtest_X, y, backtest_y_info["True_reg"])
+
+    # create the predicted X-train data sets by k-fold prediction of
+    # each single model to train the ensemble model
     pred_train = []
-    pred_train.append(predict_by_cv(X_train_Ridge78, y_train, estimator=models[0]))
-    pred_train.append(predict_by_cv(X_train_Ridge78, y_train, estimator=models[1]))
-    pred_train.append(predict_by_cv(X_train_Lasso65, y_train, estimator=models[2]))
-    pred_train.append(predict_by_cv(X_train_Ridge78, y_train, estimator=models[3]))
-    pred_train.append(predict_by_cv(X_train, y_train, estimator=models[4]))
-    pred_train.append(predict_by_cv(X_train_RFR78, y_train, estimator=models[5]))
-    pred_train.append(predict_by_cv(X_train_RFR48, y_train, estimator=models[6]))
+    pred_train.append(predict_by_cv(X_train_Ridge78, y, estimator=single[0]))
+    pred_train.append(predict_by_cv(X_train_Ridge78, y, estimator=single[1]))
+    pred_train.append(predict_by_cv(X_train_Lasso65, y, estimator=single[2]))
+    pred_train.append(predict_by_cv(X_train_Ridge78, y, estimator=single[3]))
+    pred_train.append(predict_by_cv(X, y, estimator=single[4]))
+    pred_train.append(predict_by_cv(X_train_RFR78, y, estimator=single[5]))
+    pred_train.append(predict_by_cv(X_train_RFR48, y, estimator=single[6]))
     X_train_pred = pd.DataFrame(pred_train).T
 
-    model = model_test(X_train_pred, X_pred, y_train, y_test)
+    # train the ensemble model
+    ensemble.fit(X_train_pred, y)
 
-    estimator = []
-    for a in [10, 50, 100, 200, 400, 600, 800, 1000, 2000]:
-        # estimator.append([{'alpha':a}, Ridge(alpha=a)])
-        # estimator.append([{'alpha':a}, Lasso(alpha=a)])
-        # estimator.append([{'C':a}, LinearSVR(C=a)])
-        estimator.append([{'n_estimators': a, 'n_jobs':2, 'max_features':'auto'},
-                          ExtraTreesRegressor(n_estimators=a, n_jobs=2, max_features='auto')])
-    # estimator = [('None', LinearRegression())]
-    for e in estimator:
-        easy_search(X_train_pred, X_pred, y_train, y_test, e)
-
-    svr_search(X_train_pred, X_pred, y_train, y_test)
-    etr_search(X_train_pred, X_pred, y_train, y_test)
-    rfr_search(X_train_pred, X_pred, y_train, y_test)
-
+    # test
+    pred_list = model_predict(X_test_Ridge78, X_test_Lasso65, X_test_RFR78, X_test_RFR48,
+                              X, backtest_X, y, backtest_y_info["True_reg"], models)
+    X_test_pred = pd.DataFrame(pred_list).T
+    ensemble.score(X_test_pred, backtest_y_info["True_reg"])
--- ml_q/regression/ensemble_model.py
+++ ml_q/regression/ensemble_model.py
@@ -4,6 +4,9 @@
 import numpy as np
 import pandas as pd
 import datetime, time
+from Quant.ml_q import get_data
+from Quant.ml_q.regression.feature_selection import get_best_subset
+from Quant.ml_q.regression.param_search import easy_search, svr_search, etr_search, rfr_search
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.svm import LinearSVR, SVR
@@ -11,7 +14,6 @@ from sklearn.metrics import r2_score
 
 
 def training_model(X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR48,
-                   X_test_Ridge78, X_test_Lasso65, X_test_RFR78, X_test_RFR48,
                    X_train, X_test, y_train, y_test):
 
     models = {}
@@ -55,6 +57,11 @@ def training_model(X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR4
     t8 = time.time()
     print 'RFR has fitted. Spent %0.2f min' % ((t8 - t7) / 60)
 
+    return models
+
+
+def model_predict(X_test_Ridge78, X_test_Lasso65, X_test_RFR78, X_test_RFR48,
+                   X_train, X_test, y_train, y_test, models):
     pred_list = []
     pred_list.append(models['LR'].predict(X_test_Ridge78))
     print 'Single model test score:\nLR: %s' % r2_score(y_test, pred_list[0])
@@ -77,7 +84,7 @@ def training_model(X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR4
     pred_list.append(models['RFR'].predict(X_test_RFR48))
     print 'RFR: %s' % r2_score(y_test, pred_list[6])
 
-    return models, pred_list
+    return pred_list
 
 
 def predict_by_cv(X, y, estimator, cv=5):
@@ -133,3 +140,65 @@ def model_test(X_train, X_test, y_train, y_test):
         print "R^2 on test set: %0.8f" % m[1].score(X_test, y_test)
         print "%s\n" % datetime.datetime.now()
     return model
+
+
+def model_selection():
+
+    X, y, backtest_X, backtest_y_info = get_data.Get_Data(type_y='reg')
+    X_train, X_test, y_train, y_test = get_data.split_by_weigh(X, y)
+    X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR48 = get_best_subset(X_train)
+    X_test_Ridge78, X_test_Lasso65, X_test_RFR78, X_test_RFR48 = get_best_subset(X_test)
+
+    single, ensemble = get_model()
+
+    models = training_model(X_train_Ridge78, X_train_Lasso65, X_train_RFR78, X_train_RFR48,
+                            X_train, X_test, y_train, y_test)
+    pred_list = model_predict(X_test_Ridge78, X_test_Lasso65, X_test_RFR78, X_test_RFR48,
+                              X_train, X_test, y_train, y_test, models)
+    X_test_pred = pd.DataFrame(pred_list).T
+
+    pred_train = []
+    pred_train.append(predict_by_cv(X_train_Ridge78, y_train, estimator=single[0]))
+    pred_train.append(predict_by_cv(X_train_Ridge78, y_train, estimator=single[1]))
+    pred_train.append(predict_by_cv(X_train_Lasso65, y_train, estimator=single[2]))
+    pred_train.append(predict_by_cv(X_train_Ridge78, y_train, estimator=single[3]))
+    pred_train.append(predict_by_cv(X_train, y_train, estimator=single[4]))
+    pred_train.append(predict_by_cv(X_train_RFR78, y_train, estimator=single[5]))
+    pred_train.append(predict_by_cv(X_train_RFR48, y_train, estimator=single[6]))
+    X_train_pred = pd.DataFrame(pred_train).T
+
+    model = model_test(X_train_pred, X_test_pred, y_train, y_test)
+
+    estimator = []
+    for a in [10, 50, 100, 200, 400, 600, 800, 1000, 2000]:
+        # estimator.append([{'alpha':a}, Ridge(alpha=a)])
+        # estimator.append([{'alpha':a}, Lasso(alpha=a)])
+        # estimator.append([{'C':a}, LinearSVR(C=a)])
+        estimator.append([{'n_estimators': a, 'n_jobs':2, 'max_features':'auto'},
+                          ExtraTreesRegressor(n_estimators=a, n_jobs=2, max_features='auto')])
+    # estimator = [('None', LinearRegression())]
+    for e in estimator:
+        easy_search(X_train_pred, X_test_pred, y_train, y_test, e)
+
+    svr_search(X_train_pred, X_test_pred, y_train, y_test)
+    etr_search(X_train_pred, X_test_pred, y_train, y_test)
+    rfr_search(X_train_pred, X_test_pred, y_train, y_test)
+
+
+def get_model():
+
+    single = [("LR", LinearRegression()),
+              ("Ridge", Ridge(alpha=0.09)),
+              ("Lasso", Lasso(alpha=0.0002)),
+              ("LSVR", LinearSVR(C=17.0)),
+              ("SVR", SVR(C=100.0, gamma=0.01, epsilon=0.1, kernel='rbf')),
+              ("ETR", ExtraTreesRegressor(n_estimators=1000, n_jobs=2,
+                                          max_features=0.5, max_depth=18,
+                                          min_samples_split=6, min_samples_leaf=4)),
+              ("RFR", RandomForestRegressor(n_estimators=1000, n_jobs=2,
+                                            max_features=0.5, max_depth=16,
+                                            min_samples_split=5, min_samples_leaf=2))]
+
+    ensemble = SVR(C=3.0, gamma=0.0005, epsilon=0.1, kernel='rbf')
+
+    return single, ensemble
